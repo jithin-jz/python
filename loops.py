ee4ad6609d738007051e185463462076a4b6d2b8
jithin <jithinjzx@gmail.com>
2025-09-18 14:44:50 +0530

    py

---
+++ loops.py
@@ -0,0 +1,19 @@
+# i = 10
+
+# while i <=100:
+#     print(i)
+#     i+=10
+
+# x = 1
+# while x <=10:
+#     print(x)
+#     x+=1
+
+# a = list(range(1,11,2))
+# print(a)
+# x = tuple(range(2,12,2))
+# print(x)
+
+# for i in range(2,11,2):
+#     print(i)
+
